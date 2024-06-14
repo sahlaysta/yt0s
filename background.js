cf930a62df6dfb3b194c06397138b47e0712e735
sahlaysta <sahlaysta>
2024-06-14 11:12:01 -0400

    fix cross-browser compatibility

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
+const api = this.browser || this.chrome;
 let menuItemsAlive = false;
-browser.runtime.onMessage.addListener((data, sender) => {
+api.runtime.onMessage.addListener((data, sender) => {
 	switch (data) {
 		case 'yt0s_enableContextMenuOptions': {
 			if (!menuItemsAlive) {
-				/*TODO*/if (false) browser.contextMenus.create({
+				/*TODO*/if (false) api.contextMenus.create({
 					id: 'yt0s_menuItem_startAt0sInCurrentTab',
 					title: 'Start at 0s',
 					contexts: ['link']
 				});
-				browser.contextMenus.create({
+				api.contextMenus.create({
 					id: 'yt0s_menuItem_startAt0sInNewTab',
 					title: 'Start at 0s in new tab',
 					contexts: ['link']
@@ -19,8 +20,8 @@ browser.runtime.onMessage.addListener((data, sender) => {
 		}
 		case 'yt0s_disableContextMenuOptions': {
 			if (menuItemsAlive) {
-				browser.contextMenus.remove('yt0s_menuItem_startAt0sInCurrentTab');
-				browser.contextMenus.remove('yt0s_menuItem_startAt0sInNewTab');
+				api.contextMenus.remove('yt0s_menuItem_startAt0sInCurrentTab');
+				api.contextMenus.remove('yt0s_menuItem_startAt0sInNewTab');
 				menuItemsAlive = false;
 			}
 			break;
@@ -28,20 +29,20 @@ browser.runtime.onMessage.addListener((data, sender) => {
 	}
 	return false;
 });
-browser.contextMenus.onClicked.addListener((info, tab) => {
+api.contextMenus.onClicked.addListener((info, tab) => {
 	switch (info?.menuItemId) {
 		case 'yt0s_menuItem_startAt0sInCurrentTab': {
 			const videoUrl = new URL(info.linkUrl);
 			videoUrl.searchParams.delete('t');
 			const videoUrl0s = videoUrl.href;
-			browser.tabs.sendMessage(tab.id, {targetLink: videoUrl0s, action: 'yt0s_perform_startAt0sInCurrentTab'});
+			api.tabs.sendMessage(tab.id, {targetLink: videoUrl0s, action: 'yt0s_perform_startAt0sInCurrentTab'});
 			break;
 		}
 		case 'yt0s_menuItem_startAt0sInNewTab': {
 			const videoUrl = new URL(info.linkUrl);
 			videoUrl.searchParams.delete('t');
 			const videoUrl0s = videoUrl.href;
-			browser.tabs.create({active: false, url: videoUrl0s, index: tab.index + 1});
+			api.tabs.create({active: false, url: videoUrl0s, index: tab.index + 1});
 			break;
 		}
 	}
