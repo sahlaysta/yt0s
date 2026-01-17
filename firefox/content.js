e909a8273095373ecb9c8df147125f0c1247a44c
sahlaysta <51031130+sahlaysta@users.noreply.github.com>
2026-01-16 19:35:35 -0500

    improved detection for live thumbnails

--- firefox/content.js
+++ firefox/content.js
@@ -1,24 +1,36 @@
 {
 	const ytLinkVerifier = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
-	function focusInOrMouseDown(targetElement) {
+	function focusInOrMouseDown(targetElem) {
 		function scrapeUrl() {
-			const parentFrame = targetElement.closest("yt-lockup-view-model");
-			if (parentFrame) {
-				const titleChildren = parentFrame.getElementsByClassName("yt-lockup-metadata-view-model__title");
-				if (titleChildren && titleChildren.length === 1) {
-					return titleChildren[0].href;
+			{
+				const elem = targetElem.closest("yt-lockup-view-model");
+				if (elem) {
+					const titleChildren = elem.getElementsByClassName("yt-lockup-metadata-view-model__title");
+					if (titleChildren && titleChildren.length === 1) {
+						return titleChildren[0].href;
+					}
 				}
 			}
 			
-			const targetHref = targetElement.href;
-			if (targetHref) {
-				if (ytLinkVerifier.test(targetHref)) {
-					return targetHref;
+			{
+				const elem = targetElem.closest("#media-container-link");
+				if (elem) {
+					return elem.href;
+				}
+			}
+			
+			{
+				const targetHref = targetElem.href;
+				if (targetHref) {
+					if (ytLinkVerifier.test(targetHref)) {
+						return targetHref;
+					}
 				}
 			}
 			
 			return null;
 		}
+		
 		const videoUrl = scrapeUrl();
 		if (videoUrl) {
 			yt0s_runtime_api.runtime.sendMessage({pushName: "yt0s_startContextMenuOptions", videoUrl: videoUrl});
